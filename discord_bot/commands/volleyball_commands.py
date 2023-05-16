7141a3216ee679c5f0bba1c08b9b2a0f581424c0
JasonHu1234 <112690474+JasonHu1234@users.noreply.github.com>
2023-05-17 01:40:48 +1000

    added last updated to volleyball commands fixes #3

--- discord_bot/commands/volleyball_commands.py
+++ discord_bot/commands/volleyball_commands.py
@@ -2,6 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 from discord_bot.exceptions.volleyball_exceptions import ImageNotFoundException
 
+from datetime import datetime
+
 class volleyball_commands(): 
     def __init__(self):
         self.URL = "https://volleyballdays.com/"
@@ -13,17 +15,27 @@ class volleyball_commands():
         soup = BeautifulSoup(page.content, "html.parser")
         # Had to hard code id = image36 for now - may be a better way to find the image
         result = soup.find(id = "image36").find("a").find("img")
+        # find last updated date
+        datelist = soup.find(id = "text17").string.split(" ")
+        dateobject = datetime.strptime(datelist[-1], '%d/%m/%Y')
+        datestring = "As of " + str(dateobject.strftime("%d %b %Y"))
+        daysago = " (" + str((datetime.today() - dateobject).days) + " days ago)"
         if result:
-            return self.URL + result['data-src']
+            return [datestring + daysago, self.URL + result['data-src']]
         else:
             raise ImageNotFoundException("Fixtures")
-
+    
     async def ranking(self):
         page = requests.get(self.LADDER_URL)
         soup = BeautifulSoup(page.content, "html.parser")
         # Had to hard code id = image32 for now - may be a better way to find the image
         result = soup.find(id = "image32").find("a").find("img")
+        # find last updated date
+        datelist = soup.find(id = "text12").string.split(" ")
+        dateobject = datetime.strptime(datelist[-1], '%d-%m-%y')
+        datestring = "As of " + str(dateobject.strftime("%d %b %Y"))
+        daysago = " (" + str((datetime.today() - dateobject).days) + " days ago)"
         if result:
-            return self.URL + result['data-src']
+            return [datestring + daysago, self.URL + result['data-src']]
         else:
             raise ImageNotFoundException("Ranking")
