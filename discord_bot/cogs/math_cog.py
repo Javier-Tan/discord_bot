6d06e28578c777861749137030340a7652576305
JasonHu1234 <112690474+JasonHu1234@users.noreply.github.com>
2023-05-18 14:00:31 +1000

    added simple rng command in math module

---
+++ discord_bot/cogs/math_cog.py
@@ -0,0 +1,20 @@
+import logging
+import discord
+from discord.ext import commands
+from discord_bot.commands.math_commands import math_commands
+
+class math(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def rng(self, ctx):
+        logging.info('Random number generator command executed.')
+        try:
+            response = await math_commands.rng()
+            await ctx.send(response)
+        except Exception as e:
+            await ctx.send("error")
+
+async def setup(bot):
+    await bot.add_cog(math(bot))
---
+++ discord_bot/commands/math_commands.py
@@ -0,0 +1,5 @@
+import random
+
+class math_commands():
+    async def rng():
+        return random.randint(1, 100)
